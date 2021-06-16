8d86b1ab5d6afbb3ef515026488b30d3536a28a8
taisei-hikawa <cmmkarubi@gmail.com>
2021-06-16 10:22:26 +0900

    Initial Commit

---
+++ EmbeddedMapViewInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmbeddedMapViewInSwiftUI
+//
+//  Created by 樋川大聖 on 2021/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
