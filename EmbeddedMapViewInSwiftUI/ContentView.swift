80096f34cdd8ed630998e3aee7128f47a3157c83
taisei-hikawa <cmmkarubi@gmail.com>
2021-06-16 10:43:12 +0900

    make EmbeddedMapView

--- EmbeddedMapViewInSwiftUI/ContentView.swift
+++ EmbeddedMapViewInSwiftUI/ContentView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct EmbeddedMapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ uiview: MKMapView, context: Context) {
+        let latitude = 35.661286, longitude = 139.7
+        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiview.setRegion(region, animated: true)
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
+struct EmbeddedMapView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        EmbeddedMapView()
     }
 }
